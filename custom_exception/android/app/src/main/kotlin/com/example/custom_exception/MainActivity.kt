25cc8f82564d06520652c6c17eb501f92d9be82c
ShekAdnan <adnansk418@gmail.com>
2022-01-22 23:47:05 +0600

    Custom Exception Handling class

---
+++ custom_exception/android/app/src/main/kotlin/com/example/custom_exception/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.custom_exception
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ custom_exception/lib/main.dart
@@ -0,0 +1,18 @@
+// Objective
+// Create Our Custom Exception Handling class
+class CustomException implements Exception {
+  String cause;
+  CustomException(this.cause);
+}
+
+void main() {
+  try {
+    throwException();
+  } on CustomException {
+    print("custom exception has been obtained");
+  }
+}
+
+throwException() {
+  throw new CustomException('This is my first custom exception');
+}
